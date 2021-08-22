4ce23b536fb5fd9192ee6dfe14f26e05d4409230
Ruben Guzman <ruben.guzman@neoris.com>
2021-08-22 03:09:13 -0600

    Add Entity

---
+++ todoruben.Common/Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace todoruben.Common.Models
+{
+    public class Todo
+    {
+        public DateTime CreatedTme { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
---
+++ todoruben.Common/Responses/Response.cs
@@ -0,0 +1,10 @@
+﻿namespace todoruben.Common.Responses
+{
+    public class Response
+    {
+        public bool IsSucess { get; set; }
+
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
